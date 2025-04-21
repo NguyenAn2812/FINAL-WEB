9e5c7e04c46617318cea96bf881df8769cd9b738
NguyenAn2812 <Nguyentrannhatan2812@gmail.com>
2025-04-21 21:39:37 +0700

    add

--- app/views/layouts/songcontainer.php
+++ app/views/layouts/songcontainer.php
@@ -1,5 +1,9 @@
 <div class="flex flex-wrap gap-4">
-  <?php foreach ($songs as $song): ?>
-    <?php include __DIR__ . '/../songs/songcard.php'; ?>
-  <?php endforeach; ?>
+  <?php if (!empty($songs) && is_array($songs)): ?>
+    <?php foreach ($songs as $song): ?>
+      <?php include __DIR__ . '/../songs/songcard.php'; ?>
+    <?php endforeach; ?>
+  <?php else: ?>
+    <p class="text-gray-400">There are no songs to display.</p>
+  <?php endif; ?>
 </div>
