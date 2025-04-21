ea5e2d72f3d345e18c2b1783438f4fbd3b46837a
vuhaipro2707 <150793688+vuhaipro2707@users.noreply.github.com>
2025-04-21 23:14:32 +0700

    playlisttest2

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -45,6 +45,38 @@
         </div>
     </div>
 
+    <?php
+    $playListDisplayId = 1;
+    $playListDisplayName = 'Dành cho bạn';
+    $playListDisplayInfo = [
+        ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+        ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+        ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+        ['https://picsum.photos/300/300?random=4', 'có gì đâu mà buồn', 'lalaland'],
+        ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+        ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+        ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+        ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM']
+    ];
+    include '../app/views/playlist/playlistdisplay.php';
+    ?>
+
+    <?php 
+        $playListDisplayId = 2;
+        $playListDisplayName = 'Nhạc trẻ sôi động';
+        $playListDisplayInfo = [
+        ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+        ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+        ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+        ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+        ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+        ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+        ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+        ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM']
+        ];
+        include '../app/views/playlist/playlistdisplay.php';
+    ?>
+
 
     <script>
         const BASE = "<?= BASE_URL ?>";
