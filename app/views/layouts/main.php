9659fae39a7db065a4df16866a1ec338ce7de663
vuhaipro2707 <150793688+vuhaipro2707@users.noreply.github.com>
2025-04-21 23:23:03 +0700

    Update main.php

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -4,8 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Wetube music</title>
-    <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font/css/materialdesignicons.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-[#121212] text-white">
 
