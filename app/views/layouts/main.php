cc3544fc20547c5570de0912948f7003cf930c31
vuhaipro2707 <150793688+vuhaipro2707@users.noreply.github.com>
2025-04-22 20:25:59 +0700

    Update main.php

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -15,37 +15,7 @@
 <body class="bg-[#121212] text-white">
 
 <div class="container-fluid">
-        <?php
-            $playListDisplayId = 1;
-            $playListDisplayName = 'Dành cho bạn';
-            $playListDisplayInfo = [
-                ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
-                ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
-                ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
-                ['https://picsum.photos/300/300?random=4', 'có gì đâu mà buồn', 'lalaland'],
-                ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
-                ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
-                ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
-                ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM']
-            ];
-            include '../app/views/playlist/playlistdisplay.php';
-        ?>
-
-        <?php 
-            $playListDisplayId = 2;
-            $playListDisplayName = 'Nhạc trẻ sôi động';
-            $playListDisplayInfo = [
-            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
-            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
-            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
-            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
-            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
-            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
-            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
-            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM']
-            ];
-            include '../app/views/playlist/playlistdisplay.php';
-        ?>
+        
     </div>
     <?php $this->insert('layouts/navbar'); ?>
 
@@ -83,7 +53,37 @@
         </div>
     </div>
 
-    
+    <?php
+            $playListDisplayId = 1;
+            $playListDisplayName = 'Dành cho bạn';
+            $playListDisplayInfo = [
+                ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+                ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+                ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+                ['https://picsum.photos/300/300?random=4', 'có gì đâu mà buồn', 'lalaland'],
+                ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+                ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+                ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+                ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM']
+            ];
+            include '../app/views/playlist/playlistdisplay.php';
+        ?>
+
+        <?php 
+            $playListDisplayId = 2;
+            $playListDisplayName = 'Nhạc trẻ sôi động';
+            $playListDisplayInfo = [
+            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM'],
+            ['https://picsum.photos/300/300?random=4', 'Lofi cực chill', 'EMINEM']
+            ];
+            include '../app/views/playlist/playlistdisplay.php';
+        ?>
 
     <script>
         const BASE = "<?= BASE_URL ?>";
