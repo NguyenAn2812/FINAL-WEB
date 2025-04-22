f586d3a7bb4154cc4315f7f5d91b802a3d5c8550
vuhaipro2707 <150793688+vuhaipro2707@users.noreply.github.com>
2025-04-22 11:03:33 +0700

    Update main.php

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -17,7 +17,7 @@
 
     <main class="p-4">
         <div id="app">
-            <?php $this->insert('layouts/songcontainer'); ?>
+            <!-- <?php $this->insert('layouts/songcontainer'); ?> -->
             <?php $this->insert('layouts/playlistcontainer'); ?>
         </div>
         <?php $this->insert('layouts/controllerbar'); ?>
