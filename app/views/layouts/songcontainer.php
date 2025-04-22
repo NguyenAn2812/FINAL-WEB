0b0041bbc919e2c148e69190ddf77ebd6cb8464b
vuhaipro2707 <150793688+vuhaipro2707@users.noreply.github.com>
2025-04-22 11:20:28 +0700

    Update songcontainer.php

--- app/views/layouts/songcontainer.php
+++ app/views/layouts/songcontainer.php
@@ -1,1 +1,6 @@
-
+<p>🔍 DEBUG: $songs có tồn tại không? <?= isset($songs) ? 'CÓ' : 'KHÔNG' ?></p>
+<div class="flex flex-wrap gap-4">
+  <?php foreach ($songs as $song): ?>
+    <?= $this->insert('songs/songcard', ['song' => $song]) ?>
+  <?php endforeach; ?>
+</div>
