e77f816f770aca68245f06ee37363fab0107a531
NguyenAN2812 <nguyentrannhatan2812@gmail.com>
2025-04-22 20:46:31 +0700

    add

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -20,9 +20,9 @@
 
     <main class="p-4">
         <div id = "app">
-        <?php $this->insert('layouts/playlistcontainer'); ?>
-        </div>
         
+        </div>
+        <?php $this->insert('layouts/playlistcontainer'); ?>
         <?php $this->insert('layouts/controllerbar'); ?>
     </main>
 
