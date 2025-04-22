78e485194176cd497c3f7604a37a643fca60b51b
vuhaipro2707 <150793688+vuhaipro2707@users.noreply.github.com>
2025-04-22 20:29:57 +0700

    Update main.php

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -24,15 +24,7 @@
         </div>
         <?php $this->insert('layouts/controllerbar'); ?>
     </main>
-    <!-- Login Modal -->
-    <div id="loginModal" class="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50 hidden">
-        <div id="loginModalContent" class="bg-[#1e1e1e] rounded-lg shadow-lg p-6 w-full max-w-md text-white relative">
-            <button onclick="closeLoginModal()" class="absolute top-2 right-3 text-xl hover:text-red-500">×</button>
-            <div id="loginFormContent">
-                <p>Loading...</p>
-            </div>
-        </div>
-    </div>
+
     <?php
             $playListDisplayId = 1;
             $playListDisplayName = 'Dành cho bạn';
@@ -64,6 +56,17 @@
             ];
             include '../app/views/playlist/playlistdisplay.php';
         ?>
+
+    <!-- Login Modal -->
+    <div id="loginModal" class="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50 hidden">
+        <div id="loginModalContent" class="bg-[#1e1e1e] rounded-lg shadow-lg p-6 w-full max-w-md text-white relative">
+            <button onclick="closeLoginModal()" class="absolute top-2 right-3 text-xl hover:text-red-500">×</button>
+            <div id="loginFormContent">
+                <p>Loading...</p>
+            </div>
+        </div>
+    </div>
+    
     <!-- Register Modal -->
     <div id="registerModal" class="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50 hidden">
         <div id="registerModalContent" class="bg-[#1e1e1e] rounded-lg shadow-lg p-6 w-full max-w-md text-white relative">
