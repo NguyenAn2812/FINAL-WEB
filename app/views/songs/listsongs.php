e46aca01e070b39b099a6384086a5abfd3bf93e4
NguyenAN2812 <nguyentrannhatan2812@gmail.com>
2025-04-24 06:57:01 +0700

    add

--- app/views/songs/listsongs.php
+++ app/views/songs/listsongs.php
@@ -8,7 +8,7 @@
         '<?= BASE_URL ?>/uploads/songs/<?= $song['filename'] ?>',
         '<?= addslashes($song['title']) ?>',
         '<?= addslashes($song['artist'] ?? 'Unknown') ?>',
-        '<?= BASE_URL ?>/uploads/songs/<?= $song['thumbnail'] ?>',
+        '<?= BASE_URL ?>/uploads/songs/thumbnails<?= $song['thumbnail'] ?>',
         <?= $song['id'] ?>
       )"
       class="flex items-center gap-3 cursor-pointer hover:bg-[#2a2a2a] p-2 rounded transition"
@@ -32,7 +32,7 @@ setCurrentPlaylist(<?= json_encode(array_map(function ($s) {
         'id' => $s['id'],
         'title' => $s['title'],
         'artist' => $s['artist'],
-        'thumbnail' => BASE_URL . '/uploads/songs/' . $s['thumbnail'],
+        'thumbnail' => BASE_URL . '/uploads/songs/thumbnails' . $s['thumbnail'],
         'file' => BASE_URL . '/uploads/songs/' . $s['filename']
     ];
 }, $songs), JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) ?>);
