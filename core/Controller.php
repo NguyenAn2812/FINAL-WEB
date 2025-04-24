63d88ba5ad5ef75f35470bc87af69a78a8680db4
NguyenAN2812 <nguyentrannhatan2812@gmail.com>
2025-04-24 07:18:25 +0700

    add

--- core/Controller.php
+++ core/Controller.php
@@ -2,21 +2,23 @@
 
 namespace Core;
 
+use League\Plates\Engine;
 use App\Models\User;
 use App\Models\Song;
 use App\Models\Playlist;
 
-class BaseController extends Controller
-{   
-    protected $view;
-    protected $userModel;
-    protected $songModel;
-    protected $playlistModel;
+class Controller
+{
+    protected Engine $view;
+    protected User $userModel;
+    protected Song $songModel;
+    protected Playlist $playlistModel;
 
     public function __construct()
     {
         $this->view = new Engine(__DIR__ . '/../views');
-        $this->view->registerFunction('asset', fn($path) => BASE_URL . '/' . ltrim($path, '/'));
+        $this->view->registerFunction('asset', fn($p) => BASE_URL . '/' . ltrim($p, '/'));
+
         $this->userModel = new User();
         $this->songModel = new Song();
         $this->playlistModel = new Playlist();
