889fde809aa138fcfebc8b423224b47ed125e7cf
loveCiForever <quanghuy71847@gmail.com>
2025-04-24 21:13:36 +0700

    tab logo, demo image, more button

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Wetube music</title>
@@ -7,25 +8,26 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
     <script src="https://cdn.tailwindcss.com"></script>
-
+    <link rel="icon" href="../../assets/images/logo.svg" />
     <link rel="stylesheet" href="../../assets/css/style.css">
 </head>
+
 <body class="bg-[#121212] text-white">
 
-<div class="container-fluid">
-        
+    <div class="container-fluid">
+
     </div>
-    <div id="navbar" >
+    <div id="navbar">
         <?php $this->insert('layouts/navbar'); ?>
     </div>
 
     <main class="p-4">
-        <div id = "app">
+        <div id="app">
         </div>
         <?php $this->insert('layouts/controllerbar'); ?>
     </main>
 
-    
+
 
     <!-- Login Modal -->
     <div id="loginModal" class="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50 hidden">
@@ -36,7 +38,7 @@
             </div>
         </div>
     </div>
-    
+
     <!-- Register Modal -->
     <div id="registerModal" class="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50 hidden">
         <div id="registerModalContent" class="bg-[#1e1e1e] rounded-lg shadow-lg p-6 w-full max-w-md text-white relative">
@@ -47,7 +49,7 @@
         </div>
     </div>
 
-    
+
     <!-- Upload Modal -->
     <div id="uploadModal" class="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50 hidden">
         <div id="uploadModalContent" class="bg-[#1e1e1e] rounded-lg shadow-lg p-6 w-full max-w-xl text-white relative">
@@ -58,11 +60,12 @@
         </div>
     </div>
 
-    
+
 
     <script>
         const BASE = "<?= BASE_URL ?>";
     </script>
-<script src="<?= BASE_URL ?>assets/js/script.js?v=<?= time() ?>"></script>
+    <script src="<?= BASE_URL ?>assets/js/script.js?v=<?= time() ?>"></script>
 </body>
+
 </html>
--- app/views/songs/songcard.php
+++ app/views/songs/songcard.php
@@ -1,24 +1,23 @@
 <div class="relative">
 
   <div class="songcard"
-       data-songcard="<?= $song['id'] ?>"
-       data-title="<?= htmlspecialchars($song['title'] ?? '') ?>"
-       data-artist="<?= htmlspecialchars($song['artist'] ?? '') ?>"
-       data-thumb="<?= BASE_URL ?>/uploads/thumbnails/<?= htmlspecialchars($song['thumbnail'] ?? '') ?>"
-       data-file="<?= BASE_URL ?>/uploads/songs/<?= htmlspecialchars($song['filename'] ?? '') ?>">
-       
+    data-songcard="<?= $song['id'] ?>"
+    data-title="<?= htmlspecialchars($song['title'] ?? '') ?>"
+    data-artist="<?= htmlspecialchars($song['artist'] ?? '') ?>"
+    data-thumb="<?= BASE_URL ?>/uploads/thumbnails/<?= htmlspecialchars($song['thumbnail'] ?? '') ?>"
+    data-file="<?= BASE_URL ?>/uploads/songs/<?= htmlspecialchars($song['filename'] ?? '') ?>">
+
     <img src="<?= BASE_URL ?>/uploads/thumbnails/<?= htmlspecialchars($song['thumbnail'] ?? '') ?>"
-         class="w-full max-h-32 object-cover rounded mb-2"
-         alt="<?= htmlspecialchars($song['title'] ?? '') ?>">
+      class="w-full max-h-32 w-full object-cover rounded mb-2"
+      alt="<?= htmlspecialchars($song['title'] ?? '') ?>">
 
     <h3 class="text-lg font-semibold"><?= htmlspecialchars($song['title'] ?? '') ?></h3>
     <p class="text-sm text-gray-400"><?= htmlspecialchars($song['artist'] ?? 'Unknown') ?></p>
   </div>
 
   <button
-      onclick="openAddToPlaylistModal(event, <?= $song['id'] ?>)"
-      class="absolute top-2 right-2 z-10 px-2 rounded"
-    >
-      &#8942;
+    onclick="openAddToPlaylistModal(event, <?= $song['id'] ?>)"
+    class="absolute top-2 right-2 z-10 px-2 rounded">
+    <img src="<?= BASE_URL ?>/image/more.png/" alt="more button" class="w-10 h-10 shadow-lg">
   </button>
 </div>
