8058cee3c34f47f0682646bade9cb3b31087f333
NguyenAN2812 <nguyentrannhatan2812@gmail.com>
2025-04-24 21:32:00 +0700

    add

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
     <script src="https://cdn.tailwindcss.com"></script>
-
+    <link rel="icon" href="../../assets/images/logo.svg" />
     <link rel="stylesheet" href="../../assets/css/style.css">
 </head>
 <body class="bg-[#121212] text-white">
@@ -25,8 +25,6 @@
         </div>
         <?php $this->insert('layouts/controllerbar'); ?>
     </main>
-
-    
 
     <!-- Login Modal -->
     <div id="loginModal" class="fixed inset-0 bg-black/60 backdrop-blur-sm flex items-center justify-center z-50 hidden">
