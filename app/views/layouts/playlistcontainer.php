a38e1d24e2fd5da2dc44000bd5746f0852c057ba
NguyenAN2812 <nguyentrannhatan2812@gmail.com>
2025-04-24 07:24:43 +0700

    add

--- app/views/layouts/playlistcontainer.php
+++ app/views/layouts/playlistcontainer.php
@@ -1,5 +1,6 @@
-<div class="flex flex-wrap gap-4">
-  <?php foreach ($playlists as $playlist): ?>
-    <?= $this->insert('playlist/playlistcard', ['playlist' => $playlist]) ?>
-  <?php endforeach; ?>
-</div>
+<?php foreach ($playlists as $playlist): ?>
+  <?= $this->insert('playlist/playlistcard', [
+      'playlist' => $playlist,
+      'asset' => fn($p) => BASE_URL . '/' . ltrim($p, '/')
+  ]) ?>
+<?php endforeach; ?>
--- app/views/playlist/playlistcard.php
+++ app/views/playlist/playlistcard.php
@@ -1,7 +1,7 @@
 <div class="bg-[#202020] rounded-xl overflow-hidden hover:bg-[#2a2a2a] transition cursor-pointer"
      onclick="loadPlaylistDisplay(<?= $playlist['id'] ?>)">
-    <img src="<?= asset('uploads/thumbnails/' . $playlist['thumbnail']) ?>" alt="<?= $playlist['name'] ?>"
-         class="w-full h-40 object-cover">
+     <img src="<?= $asset('uploads/thumbnails/' . $playlist['thumbnail']) ?>" alt="<?= $playlist['name'] ?>"
+     class="w-full h-40 object-cover">
     <div class="p-3">
         <p class="font-semibold truncate"><?= htmlspecialchars($playlist['name']) ?></p>
         <p class="text-sm text-gray-400 truncate"><?= htmlspecialchars($playlist['creator'] ?? 'Unknown') ?></p>
