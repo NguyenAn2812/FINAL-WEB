13f9a2317e53c4d55f3918653fe82a2edfe54d29
NguyenAN2812 <nguyentrannhatan2812@gmail.com>
2025-04-25 04:07:55 +0700

    add

--- app/views/layouts/songcontainer.php
+++ app/views/layouts/songcontainer.php
@@ -1,11 +1,12 @@
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-6 gap-5 justify-items-center" id="songGrid">
-  <div class="flex justify-center gap-4 mt-6" id="paginationControls">
-    <button onclick="prevPage()" class="bg-gray-600 text-white px-4 py-2 rounded disabled:opacity-50" id="prevBtn" disabled>⬅ Prev</button>
-    <button onclick="nextPage()" class="bg-gray-600 text-white px-4 py-2 rounded disabled:opacity-50" id="nextBtn">Next ➡</button>
-  </div>
+  
   <?php foreach ($songs as $song): ?>
     <div class="songcard-wrapper">
       <?= $this->insert('songs/songcard', ['song' => $song]) ?>
     </div>
   <?php endforeach; ?>
+</div>
+<div class="flex justify-center gap-4 mt-6" id="paginationControls">
+  <button onclick="prevPage()" class="bg-gray-600 text-white px-4 py-2 rounded disabled:opacity-50" id="prevBtn" disabled>⬅ Prev</button>
+  <button onclick="nextPage()" class="bg-gray-600 text-white px-4 py-2 rounded disabled:opacity-50" id="nextBtn">Next ➡</button>
 </div>
