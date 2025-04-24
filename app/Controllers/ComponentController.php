fcd8a66792323b2c08e623932580bbce2679d1e3
NguyenAN2812 <nguyentrannhatan2812@gmail.com>
2025-04-24 07:03:08 +0700

    add

--- app/Controllers/ComponentController.php
+++ app/Controllers/ComponentController.php
@@ -48,7 +48,7 @@ class ComponentController
 
             case 'home':
                 (new \App\Controllers\SongController())->showSongContainer();
-                (new \App\Controllers\SongController())->showPlaylistContainer();
+                (new \App\Controllers\PlaylistController())->showPlaylistContainer();
                 break;
             case 'playlistdisplay':
                 $id = $_GET['id'] ?? null;
