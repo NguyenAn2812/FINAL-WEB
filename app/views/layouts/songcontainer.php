e13ce4a7bfbf8f75ede8fa966e3eb6d303173031
NguyenAN2812 <nguyentrannhatan2812@gmail.com>
2025-04-25 02:20:55 +0700

    add

--- app/views/layouts/songcontainer.php
+++ app/views/layouts/songcontainer.php
@@ -1,5 +1,5 @@
 <h2 class="text-3xl font-bold text-white mb-4">Songs</h2>
-<div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
   <?php foreach ($songs as $song): ?>
     <?= $this->insert('songs/songcard', ['song' => $song]) ?>
   <?php endforeach; ?>
