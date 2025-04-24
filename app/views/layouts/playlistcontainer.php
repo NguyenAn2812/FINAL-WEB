451fc8edd8cf37915ac5cbc6825dffc08693a684
NguyenAN2812 <nguyentrannhatan2812@gmail.com>
2025-04-25 03:04:09 +0700

    add

--- app/views/layouts/playlistcontainer.php
+++ app/views/layouts/playlistcontainer.php
@@ -1,5 +1,5 @@
 <h2 class="text-3xl font-bold text-white mb-4">Playlist</h2>
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 justify-items-center">
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-7 gap-5 justify-items-center">
 
 <?php foreach ($playlists as $playlist): ?>
   <?= $this->insert('playlist/playlistcard', [
--- app/views/playlist/playlistcard.php
+++ app/views/playlist/playlistcard.php
@@ -1,5 +1,6 @@
+<div class="w-[200px] md:w-[220px] lg:w-[240px] cursor-pointer transition hover:scale-105 hover:drop-shadow-[0_0_10px_white] relative rounded overflow-hidden">
 <div 
-  class="w-[200px] md:w-[220px] lg:w-[240px] cursor-pointer transition hover:scale-105 hover:drop-shadow-[0_0_10px_white] relative rounded overflow-hidden"
+  class="playlistcard w-full h-full p-2 cursor-pointer flex flex-col justify-between"
   onclick="openPlaylistDisplay(<?= $playlist['id'] ?>)"
   data-playlistcard="<?= $playlist['id'] ?>"
 >
@@ -18,3 +19,6 @@
     <p class="text-gray-400 text-sm truncate">Playlist • <?= $playlist['creator'] ?? 'Unknown' ?></p>
   </div>
 </div>
+
+</div>
+
