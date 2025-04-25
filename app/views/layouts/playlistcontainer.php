18c4f69acb86d752b33c3237a5af9b73e08abc70
NguyenAN2812 <nguyentrannhatan2812@gmail.com>
2025-04-25 23:08:54 +0700

    add

--- app/views/layouts/playlistcontainer.php
+++ app/views/layouts/playlistcontainer.php
@@ -1,5 +1,6 @@
-<h2 class="text-3xl font-bold text-white mb-4">Playlist</h2>
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-7 gap-5 justify-items-center">
+<h2 class="text-3xl font-bold text-white mb-2">Playlist</h2>
+
+<div class="relative px-4 overflow-hidden w-full">
 
 <?php foreach ($playlists as $playlist): ?>
   <?= $this->insert('playlist/playlistcard', [
