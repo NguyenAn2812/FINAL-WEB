783a3173a9b67a2ad4bcb181964a8bc6cc256aab
NguyenAN2812 <nguyentrannhatan2812@gmail.com>
2025-04-25 21:34:54 +0700

    add

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -8,10 +8,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
     <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="icon" href="<?= BASE_URL ?>/assets/images/logo.svg" />
+    <link rel="icon" href="<?= BASE_URL ?>/assets/images/logo.svg"/>
     <link rel="stylesheet" href="<?= BASE_URL ?>/assets/css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
 </head>
 
 <body class="bg-[#121212] text-white">
