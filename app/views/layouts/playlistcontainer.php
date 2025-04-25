0aa28c426f9e43af570b39e26f4762f41d36c488
NguyenAN2812 <nguyentrannhatan2812@gmail.com>
2025-04-25 23:15:58 +0700

    add

--- app/views/layouts/playlistcontainer.php
+++ app/views/layouts/playlistcontainer.php
@@ -1,11 +1,12 @@
 <h2 class="text-3xl font-bold text-white mb-2">Playlist</h2>
 
-<div class="relative px-4 w-full">
-
+<div class="relative px-4 overflow-hidden w-full">
+<div class="grid grid-cols-5 gap-4">
 <?php foreach ($playlists as $playlist): ?>
   <?= $this->insert('playlist/playlistcard', [
       'playlist' => $playlist,
       'asset' => fn($p) => BASE_URL . '/' . ltrim($p, '/')
   ]) ?>
 <?php endforeach; ?>
+  </div>
 </div>
