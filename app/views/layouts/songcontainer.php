cbb78a7fe832de2f44ca33e6c4a5db4c015c4dee
NguyenAN2812 <nguyentrannhatan2812@gmail.com>
2025-04-25 09:41:46 +0700

    add

--- app/views/layouts/songcontainer.php
+++ app/views/layouts/songcontainer.php
@@ -12,7 +12,7 @@
   </button>
 </div>
 
-<div class="relative px-4">
+<div class="relative px-4 overflow-hidden w-full">
   <div id="songSlider" class="flex transition-transform duration-500 ease-in-out gap-6">
     <?php foreach ($songs as $song): ?>
       <div class="shrink-0 w-[250px] songcard-wrapper rounded-lg shadow-md">
@@ -23,14 +23,3 @@
 </div>
 
 
-<!-- <div class="flex justify-center gap-4 mt-6" id="paginationControls">
-  <button id="prevBtn" onclick="prevSlide()" aria-label="Previous"
-    class="w-9 h-9 flex items-center justify-center rounded-full bg-[#272727] hover:bg-[#3a3a3a] text-white transition disabled:opacity-40 disabled:cursor-not-allowed">
-    <i class="mdi mdi-chevron-left text-xl"></i>
-  </button>
-
-  <button id="nextBtn" onclick="nextSlide()" aria-label="Next"
-    class="w-9 h-9 flex items-center justify-center rounded-full bg-[#272727] hover:bg-[#3a3a3a] text-white transition disabled:opacity-40 disabled:cursor-not-allowed">
-    <i class="mdi mdi-chevron-right text-xl"></i>
-  </button>
-</div> -->
